85b193ecfa9ee181433d7db6f7dbb341ec3ba19b
roninix <airavanimanesh@gmail.com>
2021-07-08 19:41:06 +0430

    feat : Login page structure created.

--- src/main/java/JClashRoyale/Controller/PrimaryController.java
+++ src/main/java/JClashRoyale/Controller/PrimaryController.java
@@ -2,10 +2,37 @@ package JClashRoyale.Controller;
 
 import java.io.IOException;
 
+
 import JClashRoyale.Model.App;
 import javafx.fxml.FXML;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
 
 public class PrimaryController {
+    @FXML
+    private Pane titlePane;
+    @FXML
+    private ImageView btnMinimize, btnClose;
+    private double x, y;
+
+    public void init(Stage stage) {
+        titlePane.setOnMousePressed(mouseEvent -> {
+            x = mouseEvent.getSceneX();
+            y = mouseEvent.getSceneY();
+        });
+        titlePane.setOnMouseDragged(mouseEvent -> {
+            stage.setX(mouseEvent.getScreenX() - x);
+            stage.setY(mouseEvent.getScreenY() - y);
+        });
+
+        btnClose.setOnMouseClicked(mouseEvent -> stage.close());
+        btnMinimize.setOnMouseClicked(mouseEvent -> stage.setIconified(true));
+    }
 
     @FXML
     private void switchToSecondary() throws IOException {
