506d53b3f6922bdaa151c581590a656ef9dffcc9
roninix <airavanimanesh@gmail.com>
2021-07-13 16:16:54 +0430

    feat : Profile section cup and xp done.

--- src/main/java/JClashRoyale/Controller/ProfileSectionController.java
+++ src/main/java/JClashRoyale/Controller/ProfileSectionController.java
@@ -1,7 +1,10 @@
 package JClashRoyale.Controller;
 
 import JClashRoyale.Model.App;
+import JClashRoyale.Model.Player;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -17,6 +20,14 @@ public class ProfileSectionController {
     private Pane titlePane;
     @FXML
     private ImageView btnMinimize, btnClose;
+    @FXML
+    private Label cupLabel;
+    @FXML
+    private ProgressBar progressBar;
+    @FXML
+    private Label progressBarLabel;
+    @FXML
+    private Label levelLabel;
 
     private double x, y;
 
@@ -32,10 +43,36 @@ public class ProfileSectionController {
 
         btnClose.setOnMouseClicked(mouseEvent -> stage.close());
         btnMinimize.setOnMouseClicked(mouseEvent -> stage.setIconified(true));
+        cupLabel.setText(Integer.toString(Player.player.getCup()));
+        progressBarUpdate(Player.player.getXP());
     }
 
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
+    }
+
+    private void progressBarUpdate(int XP) {
+        if (XP <= 300) {
+            progressBar.setProgress((double) XP / 300);
+            progressBarLabel.setText(Integer.toString(XP) + '/' + "300");
+            levelLabel.setText("1");
+        } else if (XP <= 800) {
+            progressBar.setProgress((double) XP / 800);
+            progressBarLabel.setText(Integer.toString(XP) + '/' + "800");
+            levelLabel.setText("2");
+        } else if (XP <= 1700) {
+            progressBar.setProgress((double) XP / 1700);
+            progressBarLabel.setText(Integer.toString(XP) + '/' + "1700");
+            levelLabel.setText("3");
+        } else if (XP <= 3400) {
+            progressBar.setProgress((double) XP / 3400);
+            progressBarLabel.setText(Integer.toString(XP) + '/' + "3400");
+            levelLabel.setText("4");
+        } else if (XP <= 5900) {
+            progressBar.setProgress((double) XP / 5900);
+            progressBarLabel.setText(Integer.toString(XP) + '/' + "5900");
+            levelLabel.setText("5");
+        }
     }
 }
