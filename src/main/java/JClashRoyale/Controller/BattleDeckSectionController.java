b14c9142b344e9ad9cd1e281602004d702609ace
roninix <airavanimanesh@gmail.com>
2021-07-14 20:26:53 +0430

    feat : swapCards selection done.

--- src/main/java/JClashRoyale/Controller/BattleDeckSectionController.java
+++ src/main/java/JClashRoyale/Controller/BattleDeckSectionController.java
@@ -1,12 +1,20 @@
 package JClashRoyale.Controller;
 
 import JClashRoyale.Model.App;
+import JClashRoyale.Model.Cards.Card;
+import JClashRoyale.Model.Player;
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
+
 
 /**
  * @author Amir Iravanimanesh & Manni Moghimi
@@ -17,6 +25,35 @@ public class BattleDeckSectionController {
     private Pane titlePane;
     @FXML
     private ImageView btnMinimize, btnClose;
+    @FXML
+    private ImageView one;
+    @FXML
+    private ImageView two;
+    @FXML
+    private ImageView three;
+    @FXML
+    private ImageView four;
+    @FXML
+    private ImageView five;
+    @FXML
+    private ImageView six;
+    @FXML
+    private ImageView seven;
+    @FXML
+    private ImageView eight;
+    private ArrayList<ImageView> cards = new ArrayList<>();
+    @FXML
+    private ImageView oneSwap;
+    @FXML
+    private ImageView twoSwap;
+    @FXML
+    private ImageView threeSwap;
+    @FXML
+    private ImageView fourSwap;
+    private ArrayList<ImageView> swapCards = new ArrayList<>();
+
+    @FXML
+    private ImageView selectedCard;
 
     private double x, y;
 
@@ -32,10 +69,42 @@ public class BattleDeckSectionController {
 
         btnClose.setOnMouseClicked(mouseEvent -> stage.close());
         btnMinimize.setOnMouseClicked(mouseEvent -> stage.setIconified(true));
+        showDeck();
+        showSwapDeck();
+        swapCards.forEach(swapCard -> swapCard.setOnMouseClicked(mouseEvent -> onMousePressedAction(mouseEvent, swapCard)));
     }
 
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
+
+    private void showDeck() {
+        cards.addAll(Arrays.asList(one, two, three, four, five, six, seven, eight));
+        for (int i = 0; i < 8; i++) {
+            cards.get(i).setImage(new Image(Objects.requireNonNull(getClass().getResourceAsStream(
+                    Player.player.getDeck().get(i).getImageAddress()
+            ))));
+        }
+    }
+
+    private void showSwapDeck() {
+        swapCards.addAll(Arrays.asList(oneSwap, twoSwap, threeSwap, fourSwap));
+        int i = 0;
+        for (Card gameCard : Card.gameCards) {
+            if (!Player.player.getDeck().contains(gameCard)) {
+                swapCards.get(i).setImage(new Image(Objects.requireNonNull(getClass().getResourceAsStream(
+                        gameCard.getImageAddress()
+                ))));
+                i++;
+            }
+        }
+    }
+
+    private void onMousePressedAction(MouseEvent event,ImageView imageView){
+        if (selectedCard != null) selectedCard.setOpacity(1);
+        imageView.setOpacity(0.5);
+        selectedCard = imageView;
+    }
+
 }
