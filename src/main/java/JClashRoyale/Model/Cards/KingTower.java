5c5d602699ea09023c1e518bb3e4c4bb494a0bf1
roninix <airavanimanesh@gmail.com>
2021-07-19 04:17:35 +0430

    feat : KingTower & ArcherTower attributes added.

---
+++ src/main/java/JClashRoyale/Model/Cards/KingTower.java
@@ -0,0 +1,41 @@
+package JClashRoyale.Model.Cards;
+
+import JClashRoyale.Model.Player;
+
+/**
+ * @author Amir Iravanimanesh & Manni Moghimi
+ * @since 7/19/2021
+ */
+public class KingTower {
+    public static int getHP() {
+        switch (Player.player.getLevel()) {
+            case 1:
+                return 2400;
+            case 2:
+                return 2568;
+            case 3:
+                return 2736;
+            case 4:
+                return 2904;
+            case 5:
+                return 3096;
+        }
+        return 0;
+    }
+
+    public static int getDamage() {
+        switch (Player.player.getLevel()) {
+            case 1:
+                return 50;
+            case 2:
+                return 53;
+            case 3:
+                return 57;
+            case 4:
+                return 60;
+            case 5:
+                return 64;
+        }
+        return 0;
+    }
+}
